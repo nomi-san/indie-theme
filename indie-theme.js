65c6b45ff68833d65b7c0a1877e06673b52e584b
Nguyen Duy <wuuyi123@gmail.com>
2023-11-21 20:33:05 +0700

    Add source

---
+++ indie-theme.js
@@ -0,0 +1,8 @@
+/**
+ * @name indie-theme
+ * @description A simple and clean theme for Pengu Loader
+ * @author nomi-san
+ * @link https://github.com/nomi-san/indie-theme
+ */
+
+export * from 'https://unpkg.com/indie-theme@latest'
---
+++ src/index.js
@@ -0,0 +1,77 @@
+async function delay(t) {
+    return new Promise(r => setTimeout(r, t))
+}
+
+function toggleSocial() {
+    /** @type {HTMLDivElement} */
+    let pane = document.querySelector('.lol-social-lower-pane-container')
+    if (pane != null) {
+        pane.classList.toggle('hidden')
+        window.DataStore?.set('__social_hidden', pane.classList.contains('hidden'))
+    }
+}
+
+/** @returns {Promise<HTMLElement>} */
+function watchElement(selector, interval = 500) {
+    return new Promise(async resolve => {
+        while (true) {
+            let el = document.querySelector(selector)
+            if (el != null) {
+                resolve(el)
+                break
+            }
+            await delay(interval)
+        }
+    })
+}
+
+export async function load() {
+    watchElement('.bug-report-button')
+        .then(el => {
+            let clone = el.cloneNode(true)
+            el.style.display = 'none'
+            el.parentNode.appendChild(clone)
+            clone.addEventListener('click', (e) => {
+                e.stopPropagation()
+                e.preventDefault()
+                toggleSocial()
+            }, true)
+        })
+
+    watchElement('.lol-social-lower-pane-container')
+        .then(el => {
+            let hidden = window.DataStore?.get('__social_hidden', false)
+            if (hidden === true) {
+                el.classList.add('hidden')
+            }
+        })
+
+    // watchElement('.rcp-fe-viewport-persistent')
+    //     .then(el => {
+    //         console.log('.rcp-fe-viewport-persistent created')
+    //         let mo = new MutationObserver(mutations => {
+    //             mutations.forEach(mutation => {
+    //                 mutation.addedNodes.forEach(node => {
+    //                     if (node.nodeName === 'IFRAME') {
+    //                         /** @type {HTMLIFrameElement} */
+    //                         // todo
+    //                     }
+    //                 })
+    //             })
+    //         })
+    //         mo.observe(el, {
+    //             childList: true,
+    //             subtree: true
+    //         })
+    //     })
+
+    let url = import.meta.url
+    if (url.startsWith('https://plugins/')) {
+        import('./theme.css')
+    } else {
+        let link = document.createElement('link')
+        link.setAttribute('rel', 'stylesheet');
+        link.setAttribute('href', new URL('./theme.css', url).href);
+        document.body.appendChild(link)
+    }
+}
